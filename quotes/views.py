09a4128fa0408c99aeda4289a05ac8933b0c383a
Daniel Valdebenito <danielvaldebenito25@gmail.com>
2025-10-07 17:35:18 -0300

    Crear nuestro propio template 404 Not Found

--- quotes/views.py
+++ quotes/views.py
@@ -58,7 +58,7 @@ def days_week_with_number(request, day):
 # Si el dia se manda como texto acciona esta vista
 def days_week(request, day):
     if day not in days_of_week:
-        return HttpResponseNotFound('No hay frase para ese dia')
+        return render(request, '404.html')
     return render(request, 'quotes/day.html', {'day':day, 'message': days_of_week[day]})
    
 
